8fbe34debe488ce0fb4accc7a011a8ebde880e57
ps <wiv821253@gmail.com>
2020-08-27 21:09:10 +0900

    add solution

---
+++ prj/week4/qe3/ps_threeSumSolution.java
@@ -0,0 +1,39 @@
+package leetcode
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+
+public class Solution {
+
+  public List<List<Integer>> threeSum(int[] nums) {
+    var result = new ArrayList<List<Integer>>();
+    var isContains = new HashSet<List<Integer>>();
+    Arrays.sort(nums);
+
+    for (int i = 0; i < nums.length - 2; i++) {
+      int left_idx = i + 1, right_idx = nums.length - 1;
+
+      while (left_idx < right_idx) {
+        var sum = nums[i] + nums[left_idx] + nums[right_idx];
+        if (sum < 0) {
+          left_idx++;
+        } else if (sum > 0) {
+          right_idx--;
+        } else {
+          final List<Integer> anInt = List.of(nums[i], nums[left_idx], nums[right_idx]);
+          if (!isContains.contains(anInt)) {
+            result.add(anInt);
+            isContains.add(anInt);
+          }
+
+          left_idx++;
+          right_idx--;
+        }
+      }
+    }
+    return result;
+  }
+}
